f0193f84c9960d9058cdfcbf626384d0d5a06d1c
Abhay Mone <moneabhay@gmail.com>
2022-08-16 00:32:02 -0400

    updated extract function to write output to dataframe

--- extractFunctions/gmailExtract.py
+++ extractFunctions/gmailExtract.py
@@ -15,17 +15,21 @@ def extractUnreadEmails(creds):
         message_ids = service.users().messages().list(userId ='me', labelIds = ['UNREAD'], maxResults = 500).execute().get('messages',[])
         if len(message_ids) > 0 :
             msg_ids = []
+            msglist = []
             for msg in message_ids :
                 msg_id = msg['id']
                 msg_ids.append(msg_id)
                 payload = service.users().messages().get(userId = 'me', id = msg_id).execute()
-                print(payload['id'])
-                print(payload['labelIds'])
+                id = payload['id']
+                labelIds = payload['labelIds']
                 for headers in payload['payload']['headers']:
                     if headers['name'] == 'Date' :
-                        print(headers['value'])
+                        dt = headers['value']
                     elif headers['name'] == 'From' :
-                        print(headers['value'])
+                        frm = headers['value']
+                msglist.append([msg_id,id,labelIds,dt,frm])
+            df = pd.DataFrame(msglist,columns=['msg_id','payloadid','labels','receivedDt','from'])
+            df.to_csv('data/extract.csv', encoding='utf-8')
         else : 
             print("No emails to read")
             return
@@ -36,6 +40,7 @@ def extractUnreadEmails(creds):
         response =  service.users().messages().batchModify(userId = 'me', body = unreadAction).execute()
         print(response)
         """
+
     
     except HttpError as error:
         # TODO(developer) - Handle errors from gmail API.
