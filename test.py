b9b9a47c47c87c7f3007c2f0802b2147f2d8d681
Abhay Mone <moneabhay@gmail.com>
2022-10-22 11:32:57 -0400

    edited dag

--- test.py
+++
@@ -1,7 +0,0 @@
-from gmailAuthentication import authentication
-from gmailExtract import    gmailExtract
-
-
-obj = authentication()
-obj1 = gmailExtract(obj.getgmailAuthentication())
-obj1.extractUnreadEmails()
